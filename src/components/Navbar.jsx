6f79644dad6816f1ea1fd5a63b7598fdfec0306a
Abdul Saboor <saboora570@gmail.com>
2023-07-29 08:58:50 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className='navbar'>
     
     <div className='links'>
-    <Link to="/">Shop</Link>
+    <Link to="/">Shop App</Link>
     <Link to="/cart">
      <ShoppingCart size={32} />
     </Link>
